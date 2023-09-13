da4ea43443956a53f67c1368cad2016577b52898
Alex Liu <3130806899@qq.com>
2023-09-13 17:18:30 +0800

    fix Error

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -118,7 +118,7 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          Alex Liu's Chat Bot
+          Alex Liu&apos;s Chat Bot
         </div>
         <div className={styles["sidebar-sub-title"]}>
           A mirror site of ChatGPT 
