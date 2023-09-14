52e041248892beb47959e81f4d19a251ceef2f7b
Alex Liu <3130806899@qq.com>
2023-09-14 11:25:17 +0800

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,21 +21,28 @@ export const metadata: Metadata = {
     title: "Alex Liu's Chat Bot",
     statusBarStyle: "default",
   },
-  "og:type" : "website",
-  "og:title" : "Alex Liu's Chat Bot",
-  "og:description" : "Alex Liu's Chat Bot",
-  "og:image" : "/apple-touch-icon.png"
-};
+ };
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const ogType = "website";
+  const ogTitle = "Alex Liu's Chat Bot";
+  const ogDescription = "Alex Liu's Chat Bot";
+  const ogImage = "/apple-touch-icon.png";
+  const appleTouchIcon = "/apple-touch-icon.png";
+
   return (
     <html lang="en">
       <head>
         <meta name="config" content={JSON.stringify(getClientConfig())} />
+        <meta property="og:type" content={ogType} /> {/* 添加自定义的<meta>标签 */}
+        <meta property="og:title" content={ogTitle} /> {/* 添加自定义的<meta>标签 */}
+        <meta property="og:description" content={ogDescription} /> {/* 添加自定义的<meta>标签 */}
+        <meta property="og:image" content={ogImage} /> {/* 添加自定义的<meta>标签 */}
+        <link rel="apple-touch-icon" sizes="180x180" href={appleTouchIcon} /> {/* 添加自定义的<link>标签 */}
         <link rel="manifest" href="/site.webmanifest"></link>
         <script src="/serviceWorkerRegister.js" defer></script>
       </head>
